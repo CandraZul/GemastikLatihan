4bd7faf266e7bb55ffad3095275ed2fd7ea72470
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2023-09-07 12:35:20 +0700

    membuat insertion sort

--- src/algoritma/InsertionSort.java
+++ src/algoritma/InsertionSort.java
@@ -4,12 +4,52 @@
  */
 package algoritma;
 
+import java.util.Arrays;
+
 /**
  *
  * @author candr
  */
 public class InsertionSort {
-    public int Ascending (){
-        for (int i)
+    public int[] Ascending (int[] num){
+        int[] sortedNum = Arrays.copyOf(num, num.length);
+        
+        for (int i=0;i<sortedNum.length;i++){
+            int j=i;
+            while (j>0 && sortedNum[j-1]>sortedNum[j]){
+                int temp = sortedNum[j];
+                sortedNum[j] = sortedNum[j-1];
+                sortedNum[j-1] = temp;
+                
+                j--;
+            }   
+        }
+        return sortedNum;
+    }
+    
+    public int[] Descending (int[] num){
+        int[] sortedNum = Arrays.copyOf(num, num.length);
+        for (int i=0;i<sortedNum.length;i++){
+            int j=i;
+            while (j>0 && sortedNum[j-1]<sortedNum[j]){
+                int temp = sortedNum[j];
+                sortedNum[j] = sortedNum[j-1];
+                sortedNum[j-1] = temp;
+
+                j--;
+            }   
+        }
+        return sortedNum;
+}
+    
+    
+    public static void main(String[] args) {
+        int[] a= {8,2,3,1,4,3};
+        InsertionSort A = new InsertionSort();
+        
+        int[] sortedNum = A.Descending(a);
+        System.out.println(Arrays.toString(sortedNum));
+        System.out.println(Arrays.toString(a));
     }
 }
+
